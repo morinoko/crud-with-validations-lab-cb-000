48986641a85022241503300413773facfbb9f282
Felice Forby <feliceforby@gmail.com>
2018-10-11 23:01:54 +0000

    set up song form

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,7 +15,7 @@ class SongsController < ApplicationController
   def create
     @song = Song.new(song_params)
 
-    if @song.valid
+    if @song.valid?
       @song.save
       redirect_to song_path(@song)
     else
